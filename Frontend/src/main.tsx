eabce9f4c90ea1659f8df1511f300912d9174874
11Warrior <aayushthapa640@gmail.com>
2026-01-21 23:08:51 +0530

    File Cleanup

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -3,14 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
-import UserContext from '../context/UserContext.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <UserContext>
-        <App />
-      </UserContext>
+      <App />
     </BrowserRouter>
   </StrictMode>,
 )
